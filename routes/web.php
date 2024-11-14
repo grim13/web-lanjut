e815765c9fb1f966ef01d34d77927391a632ff23
Muhammad Bunyamin <muhammad.bunyamin@radenfatah.ac.id>
2024-11-14 15:58:14 +0700

    git add .

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', '\App\Http\Controllers\HomeController@index');
+Route::get('/users', '\App\Http\Controllers\UserController@index');
 Route::get('/mahasiswa', '\App\Http\Controllers\MahasiswaController@index');
